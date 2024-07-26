f1e14e22174ed131bdac73e6b0fab9bfff076a7f
BorisWeinfurt <boris@wft.cz>
2024-07-26 12:27:47 -0700

    cleanup imports

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-import elvos
 import time
+from elvos import (
+    Connection,
+    EthernetAddress,
+    MyCoolMachine,
+    MyCoolMachineNode,
+    Config,
+    Instant,
+    IpAddress,
+    State
+)
 
 
 def main():
-    connection = elvos.Connection()
+    connection = Connection()
     
-    addr1 = elvos.EthernetAddress([0x02, 0x0, 0x0, 0x0, 0x0, 0x01])
-    machine1 = elvos.MyCoolMachine(addr1, connection, "left")
-    node1 = elvos.MyCoolMachineNode(machine1)
-    node1.add_iface(elvos.Config(addr1), elvos.Instant.now())
-    node1.update_ip_addr(elvos.IpAddress.v4(1,2,3,4),24)
-    node1.add_ipv4_route([1,2,3,100])
+    addr1 = EthernetAddress([0x02, 0x0, 0x0, 0x0, 0x0, 0x01])
+    machine1 = MyCoolMachine(addr1, connection, "left")
+    node1 = MyCoolMachineNode(machine1)
+    node1.add_iface(Config(addr1), Instant.now())
+    node1.update_ip_addr(IpAddress.v4(1, 2, 3, 4), 24)
+    node1.add_ipv4_route([1, 2, 3, 100])
     
-    addr2 = elvos.EthernetAddress([0x02, 0x0, 0x0, 0x0, 0x0, 0x02])
-    machine2 = elvos.MyCoolMachine(addr2, connection, "right")
-    node2 = elvos.MyCoolMachineNode(machine2)
-    node2.add_iface(elvos.Config(addr2), elvos.Instant.now())
-    node2.update_ip_addr(elvos.IpAddress.v4(1,2,3,5),24)
-    node2.add_ipv4_route([1,2,3,100])
+    addr2 = EthernetAddress([0x02, 0x0, 0x0, 0x0, 0x0, 0x02])
+    machine2 = MyCoolMachine(addr2, connection, "right")
+    node2 = MyCoolMachineNode(machine2)
+    node2.add_iface(Config(addr2), Instant.now())
+    node2.update_ip_addr(IpAddress.v4(1, 2, 3, 5), 24)
+    node2.add_ipv4_route([1, 2, 3, 100])
     
     client_handle = node1.add_tcp_socket()
     server_handle = node2.add_tcp_socket()
     
-    remode_addr = elvos.IpAddress.v4(1,2,3,5)
+    remote_addr = IpAddress.v4(1, 2, 3, 5)
     remote_port = 1234
     host_port = 65000
     
     while True:
         print("Polling Client")
-        node1.poll(elvos.Instant.now())
+        node1.poll(Instant.now())
         print("Polling Server")
-        node2.poll(elvos.Instant.now())
+        node2.poll(Instant.now())
         
         print("Client Side")
         client_state = node1.socket_status(client_handle)
@@ -39,13 +48,14 @@ def main():
 
         node2.start_http_server(server_handle, remote_port)
         
-        node1.start_http_client(client_handle, remode_addr, remote_port, host_port)
+        node1.start_http_client(client_handle, remote_addr, remote_port, host_port)
 
-        if client_state == elvos.State.Established and server_state == elvos.State.Established:
+        if client_state == State.Established and server_state == State.Established:
             node1.send_request(client_handle, "GET", "/elvis.html")
             node2.handle_http_server(server_handle)
 
         time.sleep(1)
 
 
-main()
+if __name__ == "__main__":
+    main()
